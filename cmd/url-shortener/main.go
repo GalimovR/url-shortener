691a9e7b31daf682aa473ff4ae696403fd048e9a
mariaele <mariaele@student.21-school.ru>
2024-11-30 19:50:18 +0300

    Feat: initialize project and config

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "log/slog"
+)
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi
+
+	// TODO: start server:
+}
